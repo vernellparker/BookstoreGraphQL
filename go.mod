98fcd38f6b65b017690c7e2ef9fab51101a976d1
vernellparker <vernell.parker@gmail.com>
2020-06-10 23:39:08 -0400

    Added Auth

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/99designs/gqlgen v0.11.3
+	github.com/appleboy/gin-jwt/v2 v2.6.3
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.6.1 // indirect
--- server.go
+++ server.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 	"github.com/vernellparker/BookstoreGraphQL/handlers/graphqlHandlers"
+	"github.com/vernellparker/BookstoreGraphQL/middleware"
 	"log"
 )
 
 func main() {
-	//Sets up gin to handle requests. Gin is also a 3rd party that allows for better HTTP, middleware, logging etc
-	r := gin.Default()
-	r.POST("/query", graphqlHandlers.GraphqlHandler())
+	//Sets up gin to handle requests. Gin is also a 3rd party that allows for better HTTP, middleware, logging etc an
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	authMiddleware, err := middleware.JwtHandler()
+
+	if err != nil {
+		log.Fatal("JWT Error:" + err.Error())
+	}
+
+	r.POST("/login", authMiddleware.LoginHandler)
 	r.GET("/", graphqlHandlers.PlaygroundHandler())
+
+	r.NoRoute(authMiddleware.MiddlewareFunc(), func(c *gin.Context) {
+		claims := jwt.ExtractClaims(c)
+		log.Printf("NoRoute claims: %#v\n", claims)
+		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	})
+
+
+	auth := r.Group("/auth")
+	// Refresh time can be longer than token timeout
+	auth.GET("/refresh_token", authMiddleware.RefreshHandler)
+
+	auth.Use(authMiddleware.MiddlewareFunc())
+	{
+		auth.POST("/query", graphqlHandlers.GraphqlHandler())
+	}
 
 	if err := r.Run(); err != nil {
 		log.Fatal(err)
